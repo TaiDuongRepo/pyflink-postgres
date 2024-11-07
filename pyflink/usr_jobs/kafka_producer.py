c97cbfa75c2d4b421e1f22fc3c9c9caff7298ca0
TaiDuongRepo <taiduong1301@gmail.com>
2024-11-07 10:52:32 +0700

    update

--- pyflink/usr_jobs/kafka_producer.py
+++ pyflink/usr_jobs/kafka_producer.py
@@ -9,13 +9,83 @@ from kafka import KafkaProducer
 
 SLEEP_TIME = 3
 
+
 def generate_sensor_data() -> dict[str, Any]:
     """Generates random sensor data. It also adds a timestamp for traceability."""
-    message_id = uuid.uuid4().hex
     sensor_data = {
-        "message_id": message_id,
-        "content": "Xe Thaco đi bao êm bao mượt",
-        "title": "Xe của Thaco đi chán, ông Trần Bá Dương làm ăn kém"
+        "url": "https://www.youtube.com/watch?v=btUbLgsftKc",
+        "address": None,
+        "brand": "thaco auto",
+        "comment_count": 2,
+        "comments": [
+            {
+                "created_at": "2023-04-16T02:47:02Z",
+                "reply_to": None,
+                "user_name": "@hoaikhangphan6350",
+                "nickname": "@hoaikhangphan6350",
+                "user_id": "UC3SJzlHeA43kl0qGhUSji4w",
+                "email": None,
+                "phone": None,
+                "address": None,
+                "title": None,
+                "content": "Bn 1 chiếc ạ",
+                "reaction": {
+                    "like": 0,
+                    "love": None,
+                    "haha": None,
+                    "wow": None,
+                    "sad": None,
+                    "angry": None,
+                    "other": None,
+                },
+                "id": "Ugw1V0Dr0ulPjl-yiJR4AaABAg",
+            },
+            {
+                "created_at": "2022-12-22T12:24:23Z",
+                "reply_to": None,
+                "user_name": "@LongThanh-zw8zv",
+                "nickname": "@LongThanh-zw8zv",
+                "user_id": "UC3SJzlHeA43kl0qGhUSji4w",
+                "email": None,
+                "phone": None,
+                "address": None,
+                "title": None,
+                "content": "Giá lăn bánh bao nhiêu ak",
+                "reaction": {
+                    "like": 0,
+                    "love": None,
+                    "haha": None,
+                    "wow": None,
+                    "sad": None,
+                    "angry": None,
+                    "other": None,
+                },
+                "id": "Ugw3mJ7-pp0aXaeckxl4AaABAg",
+            },
+        ],
+        "description": "THACO BLUE SKY 120S -XE BUS GHẾ NGỒI HIỆN ĐẠI, SANG TRỌNG\nDòng xe bus ghế ngồi cao cấp (28 chỗ & 47 chỗ), kiểu dáng Universe hiện đại, nội thất sang trọng",
+        "email": None,
+        "favorite_count": 0,
+        "id": "btUbLgsftKc",
+        "keyword": ["#thaco"],
+        "media_channel": "youtube",
+        "phone": None,
+        "reaction": {
+            "like": 50,
+            "love": None,
+            "haha": None,
+            "wow": None,
+            "sad": None,
+            "angry": None,
+            "other": None,
+        },
+        "share": None,
+        "social_type": "youtube",
+        "title": "Xe khách 47 ghế cao cấp của Thaco. Trải nghiệm và đánh giá nhanh xe Bluesky TB120S |LH: 0911 102 664",
+        "user_id": "UC3SJzlHeA43kl0qGhUSji4w",
+        "user_name": "Mẫn Xe Khách",
+        "view": 7054,
+        "created_at": "2022-12-11T08:05:44Z",
     }
     return sensor_data
 
--- pyflink/usr_jobs/postgres_sink.py
+++ pyflink/usr_jobs/postgres_sink.py
@@ -1,6 +1,7 @@
 import json
 import logging
 from datetime import datetime
+import uuid
 
 from pyflink.common import Row, Types, WatermarkStrategy
 from pyflink.common.serialization import SimpleStringSchema
@@ -22,30 +23,108 @@ from transformers import pipeline
 KAFKA_HOST = "kafka:19092"
 POSTGRES_HOST = "postgres:5432"
 
-# sentiment_pipeline = pipeline("text-classification", model="5CD-AI/Vietnamese-Sentiment-visobert")
-
-def parse_data(data: str) -> Row:
-    sentiment_pipeline = pipeline("text-classification", model="5CD-AI/Vietnamese-Sentiment-visobert")
+def parse_date_data(data: str) -> Row:
     data = json.loads(data)
-    message_id = data["message_id"]
+    id = uuid.uuid4().hex
+    created_at = data["created_at"]
+    full_date = datetime.strptime(created_at, "%Y-%m-%dT%H:%M:%SZ").date()
+    weekday = datetime.strptime(created_at, "%Y-%m-%dT%H:%M:%SZ").strftime("%A")
+    weekmonth = (datetime.strptime(created_at, "%Y-%m-%dT%H:%M:%SZ").day - 1) // 7 + 1
+    day = datetime.strptime(created_at, "%Y-%m-%dT%H:%M:%SZ").day
+    month = datetime.strptime(created_at, "%Y-%m-%dT%H:%M:%SZ").month
+    year = datetime.strptime(created_at, "%Y-%m-%dT%H:%M:%SZ").year
+    quarter = (datetime.strptime(created_at, "%Y-%m-%dT%H:%M:%SZ").month - 1) // 3 + 1
+    return Row(id, full_date, weekday, weekmonth, day, month, year, quarter)
+
+def parse_time_data(data: str) -> Row:
+    data = json.loads(data)
+    id = uuid.uuid4().hex
+    full_time = data["created_at"]
+    hour = datetime.strptime(full_time, "%Y-%m-%dT%H:%M:%SZ").hour
+    minute = datetime.strptime(full_time, "%Y-%m-%dT%H:%M:%SZ").minute
+    second = datetime.strptime(full_time, "%Y-%m-%dT%H:%M:%SZ").second
+    am_pm = "AM" if hour < 12 else "PM"
+    shift = "Morning" if 4 <= hour < 12 else "Afternoon" if 12 <= hour < 20 else "Night"
+    return Row(id, full_time, hour, minute, second, am_pm, shift)
+
+def parse_fact_content_data(data: str) -> Row:
+    data = json.loads(data)
+    id = uuid.uuid4().hex
+    sentiment_analyzer = pipeline("text-classification", model="5CD-AI/Vietnamese-Sentiment-visobert")
+    sentiment_score = sentiment_analyzer(data["content"])[0]["score"]
+    post_id = data["post_id"]
+    comment_id = data["comment_id"]
+    return Row(id, sentiment_score, post_id, comment_id)
+
+def parse_social_data(data: str) -> Row:
+    data = json.loads(data)
+    id = data["id"]
+    name = data["social_type"]
+    brand = data["brand"]
+    return Row(id, name, brand)
+
+def parse_user_data(data: str) -> Row:
+    data = json.loads(data)
+    id = uuid.uuid4().hex
+    user_id = data["user_id"]
+    name = data["user_name"]
+    handle = ""
+    email = data["email"]
+    phone = data["phone"]
+    address = data["address"]
+    return Row(id, user_id, name, handle, email, phone, address)
+
+def parse_post_data(data: str) -> Row:
+    data = json.loads(data)
+    id = uuid.uuid4().hex
+    post_id = data["id"]
+    user_id = data["user_id"]
+    social_id = 1
+    created_at = data["created_at"]
+    created_date = datetime.strptime(created_at, "%Y-%m-%dT%H:%M:%SZ").date()
+    created_time = datetime.strptime(created_at, "%Y-%m-%dT%H:%M:%SZ").time()
+    url = data["url"]
     title = data["title"]
     content = data["content"]
-    sentiment = sentiment_pipeline(title)[0]['label']
-    # sentiment = "POS"
+    sentiment_score = 0.0
+    count_comment = data["count_comment"]
+    count_reaction = 0
+    count_like = 0
+    count_dislike = 0
+    count_view = data["view"]
+    return Row(id, post_id, user_id, social_id, created_date, created_time, url, title, content, sentiment_score, count_comment, count_reaction, count_like, count_dislike, count_view)
 
-    return Row(message_id, title, content, sentiment)
+def parse_comment_data(data: str) -> Row:
+    data = json.loads(data)
+    comments = data["comments"]
+    for comment in comments:
+        id = uuid.uuid4().hex
+        comment_id = comment["id"]
+        user_id = comment["user_id"]
+        created_date = datetime.strptime(comment["created_at"], "%Y-%m-%dT%H:%M:%SZ").date()
+        created_time = datetime.strptime(comment["created_at"], "%Y-%m-%dT%H:%M:%SZ").time()
+        url = ""
+        content = comment["content"]
+        sentiment_score = 0
+        count_react = 0
+        count_like = comment["reaction"]["like"]
+        count_dislike = 0
+        yield Row(id, comment_id, user_id, created_date, created_time, url, content, sentiment_score, count_react, count_like, count_dislike)
+
+def parse_hashtag_data(data: str) -> Row:
+    data = json.loads(data)
+    id = uuid.uuid4().hex
+    hashtags = data["keyword"][0]
+    return Row(id, hashtags)
+
+def parse_fact_posttag_data(data: str) -> Row:
+    data = json.loads(data)
+    id = uuid.uuid4().hex
+    post_id = data["post_id"]
+    hashtag_id = data["hashtag_id"]
+    return Row(id, post_id, hashtag_id)
 
 
-def filter_temperatures(value: str) -> str | None:
-    data = json.loads(value)
-    message_id = data["message_id"]
-    title = data["title"]
-    alert_message = {
-        "message_id": message_id,
-        "title": title,
-    }
-    return json.dumps(alert_message)
-    # return None
 
 
 def initialize_env() -> StreamExecutionEnvironment:
@@ -135,20 +214,134 @@ def main() -> None:
     # Define source and sinks
     logger.info("Configuring source and sinks")
     kafka_source = configure_source(KAFKA_HOST)
-    sql_dml = (
-        "INSERT INTO raw_sensors_data (message_id, title, content, sentiment) "
+    sql_dml_date = (
+        "INSERT INTO date_data (full_data, weekday, weekmonth, day, month, year, quarter) "
+        "VALUES (?, ?, ?, ?, ?, ?, ?)"
+    )
+    sql_dml_time = (
+        "INSERT INTO time_data (full_time, hour, minute, second, am_pm, shift) "
+        "VALUES (?, ?, ?, ?, ?, ?)"
+    )
+    sql_dml_fact_content = (
+        "INSERT INTO fact_content (id, sentiment_score, post_id, comment_id) "
         "VALUES (?, ?, ?, ?)"
     )
-    TYPE_INFO = Types.ROW(
+    sql_dml_social = (
+        "INSERT INTO social_data (id, name, brand) "
+        "VALUES (?, ?, ?)"
+    )
+    sql_dml_user = (
+        "INSERT INTO user_data (user_id, name, handle, email, phone, address) "
+        "VALUES (?, ?, ?, ?, ?, ?)"
+    )
+    sql_dml_post = (
+        "INSERT INTO post_data (post_id, user_id, social_id, url, title, content) "
+        "VALUES (?, ?, ?, ?, ?, ?)"
+    )
+    sql_dml_comment = (
+        "INSERT INTO comment_data (comment_id, user_id, created_date, created_time, url, content, sentiment_score, count_react, count_like, count_dislike) "
+        "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+    )
+    sql_dml_hashtag = (
+        "INSERT INTO hashtag_data (id, hashtags) "
+        "VALUES (?, ?)"
+    )
+    sql_dml_fact_posttag = (
+        "INSERT INTO fact_posttag (id, post_id, hashtag_id) "
+        "VALUES (?, ?, ?)"
+    )
+    
+    TYPE_INFO_DATE = Types.ROW(
         [
-            Types.STRING(),  # message_id
-            Types.STRING(),  # title
-            Types.STRING(),  # content
-            Types.STRING(),  # sentiment
+            Types.STRING(),  # full_data
+            Types.STRING(),  # weekday
+            Types.STRING(),  # weekmonth
+            Types.INT(),  # day
+            Types.INT(),  # month
+            Types.INT(),  # year
+            Types.INT(),  # quarter
         ]
     )
-    jdbc_sink = configure_postgre_sink(sql_dml, TYPE_INFO)
-    kafka_sink = configure_kafka_sink(KAFKA_HOST, "alerts")
+    TYPE_INFO_TIME = Types.ROW(
+        [
+            Types.STRING(),  # full_time
+            Types.INT(),  # hour
+            Types.INT(),  # minute
+            Types.INT(),  # second
+            Types.STRING(),  # am_pm
+            Types.STRING(),  # shift
+        ]
+    )
+    TYPE_INFO_FACT_CONTENT = Types.ROW(
+        [
+            Types.STRING(),  # id
+            Types.FLOAT(),  # sentiment_score
+            Types.STRING(),  # post_id
+            Types.STRING(),  # comment_id
+        ]
+    )
+    TYPE_INFO_SOCIAL = Types.ROW(
+        [
+            Types.STRING(),  # id
+            Types.STRING(),  # name
+            Types.STRING(),  # brand
+        ]
+    )
+    TYPE_INFO_USER = Types.ROW(
+        [
+            Types.STRING(),  # user_id
+            Types.STRING(),  # name
+            Types.STRING(),  # handle
+            Types.STRING(),  # email
+            Types.STRING(),  # phone
+            Types.STRING(),  # address
+        ]
+    )
+    TYPE_INFO_POST = Types.ROW(
+        [
+            Types.STRING(),  # post_id
+            Types.STRING(),  # user_id
+            Types.INT(),  # social_id
+            Types.STRING(),  # url
+            Types.STRING(),  # title
+            Types.STRING(),  # content
+        ]
+    )
+    TYPE_INFO_COMMENT = Types.ROW(
+        [
+            Types.STRING(),  # comment_id
+            Types.STRING(),  # user_id
+            Types.STRING(),  # created_date
+            Types.STRING(),  # created_time
+            Types.STRING(),  # url
+            Types.STRING(),  # content
+            Types.FLOAT(),  # sentiment_score
+            Types.INT(),  # count_react
+            Types.INT(),  # count_like
+            Types.INT(),  # count_dislike
+        ]
+    )
+    TYPE_INFO_HASHTAG = Types.ROW(
+        [
+            Types.STRING(),  # id
+            Types.STRING(),  # hashtags
+        ]
+    )
+    TYPE_INFO_FACT_POSTTAG = Types.ROW(
+        [
+            Types.STRING(),  # id
+            Types.STRING(),  # post_id
+            Types.STRING(),  # hashtag_id
+        ]
+    )
+    jdbc_sink_date = configure_postgre_sink(sql_dml_date, TYPE_INFO_DATE)
+    jdbc_sink_time = configure_postgre_sink(sql_dml_time, TYPE_INFO_TIME)
+    jdbc_sink_fact_content = configure_postgre_sink(sql_dml_fact_content, TYPE_INFO_FACT_CONTENT)
+    jdbc_sink_user = configure_postgre_sink(sql_dml_user, TYPE_INFO_USER)
+    jdbc_sink_post = configure_postgre_sink(sql_dml_post, TYPE_INFO_POST)
+    jdbc_sink_comment = configure_postgre_sink(sql_dml_comment, TYPE_INFO_COMMENT)
+    jdbc_sink_hashtag = configure_postgre_sink(sql_dml_hashtag, TYPE_INFO_HASHTAG)
+    jdbc_sink_fact_posttag = configure_postgre_sink(sql_dml_fact_posttag, TYPE_INFO_FACT_POSTTAG)
     logger.info("Source and sinks initialized")
 
     # Create a DataStream from the Kafka source and assign watermarks
@@ -157,16 +350,27 @@ def main() -> None:
     )
 
     # Make transformations to the data stream
-    transformed_data = data_stream.map(parse_data, output_type=TYPE_INFO)
-    alarms_data = data_stream.map(
-        filter_temperatures, output_type=Types.STRING()
-    ).filter(lambda x: x is not None)
+    # transformed_data = data_stream.map(parse_data, output_type=TYPE_INFO)
+    transformed_data_date = data_stream.map(parse_date_data, output_type=TYPE_INFO_DATE)
+    transformed_data_time = data_stream.map(parse_time_data, output_type=TYPE_INFO_TIME)
+    transformed_data_fact_content = data_stream.map(parse_fact_content_data, output_type=TYPE_INFO_FACT_CONTENT)
+    transformed_data_user = data_stream.map(parse_user_data, output_type=TYPE_INFO_USER)
+    transformed_data_post = data_stream.map(parse_post_data, output_type=TYPE_INFO_POST)
+    transformed_data_comment = data_stream.flat_map(parse_comment_data, output_type=TYPE_INFO_COMMENT)
+    transformed_data_hashtag = data_stream.map(parse_hashtag_data, output_type=TYPE_INFO_HASHTAG)
+    transformed_data_fact_posttag = data_stream.map(parse_fact_posttag_data, output_type=TYPE_INFO_FACT_POSTTAG)
     logger.info("Defined transformations to data stream")
 
     logger.info("Ready to sink data")
-    alarms_data.print()
-    alarms_data.sink_to(kafka_sink)
-    transformed_data.add_sink(jdbc_sink)
+    transformed_data_date.add_sink(jdbc_sink_date)
+    transformed_data_time.add_sink(jdbc_sink_time)
+    transformed_data_fact_content.add_sink(jdbc_sink_fact_content)
+    transformed_data_user.add_sink(jdbc_sink_user)
+    transformed_data_post.add_sink(jdbc_sink_post)
+    transformed_data_comment.add_sink(jdbc_sink_comment)
+    transformed_data_hashtag.add_sink(jdbc_sink_hashtag)
+    transformed_data_fact_posttag.add_sink(jdbc_sink_fact_posttag)
+    logger.info("Data sinked")
 
     # Execute the Flink job
     env.execute("Flink PostgreSQL and Kafka Sink")
